c94bbc4323c002d7a8c2ab049f15b123d327ace7
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-20 15:02:28 +0100

    structure include bombs

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,19 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
 // Creo una variabile dove salvo il container delle 100 celle (container-grid)
+function getRandomNumber(min, max) {
+    const randNumb = Math.floor(Math.random() * (max - min + 1)) + min
+
+    return randNumb;
+}
 
 const container = document.querySelector(".container");
 const playButton = document.querySelector('.play')
 playButton.addEventListener('click', function () {
     container.innerHTML = ""
+// Preparazione di Bombe e tentativi
+    let attempts = 0;
+    const totalBombs = 16;
 
     const diffucultyEl = document.querySelector('.diffuculty')
     console.log(diffucultyEl);
@@ -20,7 +28,19 @@ playButton.addEventListener('click', function () {
         const cell = `<div class="cell" style="width: calc(100% / ${Math.sqrt(nMax)}"><p class="m-0">${i}</p></div>`;
 
         container.innerHTML += cell;
+        const maxAttempts = nMax - totalBombs;
     }
+    const generateBombs = (totalBombs, nMax) => {
+        const bombs = [];
+        while (bombs.length < totalBombs) {
+            const randomNumber = getRandomNumber(1, nMax);
+            if (!bombs.includes(randomNumber)) {
+                bombs.push(randomNumber);
+            }
+        }
+        return bombs;
+    }
+
 
     // seleziono una cella che ha classe cell e active
 
